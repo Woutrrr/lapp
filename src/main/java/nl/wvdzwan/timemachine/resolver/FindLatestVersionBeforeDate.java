14c92ef9af7cb2699938096991382e45ecd2bdd7
Wouter van der Zwan <mail@wvdzwan.nl>
2018-11-28 16:01:27 +0100

    Rewrite prototype latest version finder to usable class

--- src/main/java/nl/wvdzwan/timemachine/resolver/FindLatestVersionBeforeDate.java
+++ src/main/java/nl/wvdzwan/timemachine/resolver/FindLatestVersionBeforeDate.java
@@ -1,98 +1,73 @@
 package nl.wvdzwan.timemachine.resolver;
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- * 
- *  http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+import java.time.LocalDateTime;
 
-import nl.wvdzwan.timemachine.HttpClient;
-import nl.wvdzwan.timemachine.libio.LibrariesIOClient;
-import nl.wvdzwan.timemachine.libio.LibrariesIOInterface;
-import nl.wvdzwan.timemachine.resolver.util.Booter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
 import org.eclipse.aether.DefaultRepositorySystemSession;
 import org.eclipse.aether.RepositorySystem;
-import org.eclipse.aether.RepositorySystemSession;
-import org.eclipse.aether.artifact.Artifact;
 import org.eclipse.aether.artifact.DefaultArtifact;
-import org.eclipse.aether.impl.DefaultServiceLocator;
 import org.eclipse.aether.resolution.VersionRangeRequest;
+import org.eclipse.aether.resolution.VersionRangeResolutionException;
 import org.eclipse.aether.resolution.VersionRangeResult;
 import org.eclipse.aether.version.Version;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import nl.wvdzwan.timemachine.resolver.util.Booter;
+
 
 /**
- * Determines the newest version of an artifact.
+ * Find latest version of an artifact before a specified date.
  */
-public class FindLatestVersionBeforeDate
-{
+public class FindLatestVersionBeforeDate {
 
-    protected static Logger logger = LogManager.getLogger();
+
+    private static final Logger logger = LogManager.getLogger(FindLatestVersionBeforeDate.class);
+
+    private final RepositorySystem system;
+
+    public FindLatestVersionBeforeDate(RepositorySystem system) {
+        this.system = system;
+    }
 
     /**
-     * Main.
-     * @param args
-     * @throws Exception
+     * Return latest version of an artifact before date.
+     *
+     * <p>All found versions after `date` are ignored</p>
+     *
+     * @param packageIdentifier The Artifact identifier, formatted as groupID:artifactID
+     * @param dateLimit         The date used to filter newer versions
+     * @return found version
+     * @throws VersionRangeResolutionException when
      */
-    public static void main( String[] args )
-        throws Exception
-    {
-        System.out.println( "------------------------------------------------------------" );
-        System.out.println( FindLatestVersionBeforeDate.class.getSimpleName() );
+    public Version latestBeforeDate(String packageIdentifier, LocalDateTime dateLimit)
+            throws VersionRangeResolutionException {
+        logger.debug("Find latest version of {} before {}...", packageIdentifier, dateLimit);
 
-        String apiKey = "ad19ce0d9ce33eac2bcdadb6ea73a388";
-        String datetime_limit = "2008-03-14";
-        String identifier = "com.thoughtworks.xstream:xstream";
+        DefaultRepositorySystemSession session = Booter.newRepositorySystemSession(system);
+        session.setConfigProperty(CustomVersionRangeResolver.CONFIG_LIMIT_DATE, dateLimit);
 
 
-        DefaultServiceLocator locator = MavenRepositorySystemUtils.newServiceLocator();
-        RepositorySystem system = Booter.newRepositorySystem(locator);
-
-        LibrariesIOInterface api = locator.getService(LibrariesIOInterface.class);
-        api.setApiKey( apiKey );
-
-
-        RepositorySystemSession session = Booter.newRepositorySystemSession( system );
-        ((DefaultRepositorySystemSession) session).setVersionFilter(new DateVersionFilter(api, LocalDate.parse("2010-04-04")));
-        ((DefaultRepositorySystemSession) session).setConfigProperty("time-machine.date", datetime_limit);
-        ((DefaultRepositorySystemSession) session).setConfigProperty("librariesio.key", apiKey);
-
-        Artifact artifact = new DefaultArtifact( identifier + ":[0,)" );
+        DefaultArtifact artifact = new DefaultArtifact(packageIdentifier + ":[0,)");
 
         VersionRangeRequest rangeRequest = new VersionRangeRequest();
-        rangeRequest.setArtifact( artifact );
-        rangeRequest.setRepositories( Booter.newRepositories( system, session ) );
+        rangeRequest.setArtifact(artifact);
+        rangeRequest.setRepositories(Booter.newRepositories(system, session));
 
-        VersionRangeResult rangeResult = system.resolveVersionRange( session, rangeRequest );
+
+        VersionRangeResult rangeResult = system.resolveVersionRange(session, rangeRequest);
 
         if (rangeResult.getVersions().size() == 0) {
-            logger.fatal("No suitable artifact found for {} before {}", identifier, datetime_limit);
-            return;
+            logger.error("No suitable artifact found for {} before {}", packageIdentifier, dateLimit);
+            throw new VersionRangeResolutionException(
+                    rangeResult,
+                    String.format("No suitable artifact found for %s before %s", packageIdentifier, dateLimit)
+            );
         }
 
         Version version = rangeResult.getHighestVersion();
-        logger.info("Found version: {} for {}", version.toString(), identifier);
+        logger.info("Found version {} for {} before {}", version.toString(), packageIdentifier, dateLimit);
 
-
-
+        return version;
     }
 
 }
