59fc867887991db51ac85c206741924b6c34bac6
Wouter van der Zwan <mail@wvdzwan.nl>
2019-01-04 12:12:02 +0100

    test: ensure main jar parameter is supplied
    Make sure a main jar is defined when starting from the command line. Omitting a main jar makes no sense, since what should the call graph be generated for? Not supplying a main jar parameter results in a null pointer exception somewhere in the code.

---
+++ src/test/java/nl/wvdzwan/timemachine/callgraph/CallGraphCommandTest.java
@@ -0,0 +1,21 @@
+package nl.wvdzwan.timemachine.callgraph;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import picocli.CommandLine;
+
+public class CallGraphCommandTest {
+
+    @Test
+    void requiresMainJarArgument() {
+
+        String[] args = {};
+
+        CommandLine commandLine = new CommandLine(new CallGraphMain());
+
+        Assertions.assertThrows(CommandLine.ParameterException.class, () -> {
+            commandLine.parse(args);
+        }, "Should have complained about missing parameters.");
+
+    }
+}
